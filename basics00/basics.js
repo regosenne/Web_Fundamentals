175f8888209cdea20dab9a0163fc588156acba0c
rsenne <rsenne@student.wethinkcode.co.za>
2022-01-20 19:45:42 +0200

    fixed some errors

--- basics00/basics.js
+++ basics00/basics.js
@@ -1,3 +1,18 @@
-function hackCity(event, cityName){
+function hackCity(e, cityName){
+    var counter , tabcontent, tablink, tabs;
 
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for(counter = 0; counter < tabcontent.length; counter++){
+        tabcontent[i].style.display = "none";
+    }
+    
+    tabs = document.getElementById("tabs");
+    tablink = tabs.getElementsByClassName("tablink");
+    for(counter = 0; counter < tablink.length; counter++){
+        tablink[i].className = tablink[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    e.currentTarget.className += " active";
 }
