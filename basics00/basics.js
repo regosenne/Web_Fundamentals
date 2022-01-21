cfac456f616e4e638a9a70f1f31667764ae34809
rsenne <rsenne@student.wethinkcode.co.za>
2022-01-21 08:48:45 +0200

    Js is behaving a bit now

--- basics00/basics.js
+++ basics00/basics.js
@@ -1,18 +1,17 @@
-function hackCity(e, cityName){
+function hackCity(cityName){
     var counter , tabcontent, tablink, tabs;
-
 
     tabcontent = document.getElementsByClassName("tabcontent");
     for(counter = 0; counter < tabcontent.length; counter++){
-        tabcontent[i].style.display = "none";
+        if (tabcontent[counter].style.display === "none") {
+            tabcontent[counter].style.display = "block";
+        }else {
+            tabcontent[counter].style.display = "none";
+        }
     }
     
-    tabs = document.getElementById("tabs");
-    tablink = tabs.getElementsByClassName("tablink");
+    tablink = document.getElementsByClassName("tablink");
     for(counter = 0; counter < tablink.length; counter++){
-        tablink[i].className = tablink[i].className.replace(" active", "");
+        tablink[counter].className = tablink[counter].className.replace("active", "");
     }
-
-    document.getElementById(cityName).style.display = "block";
-    e.currentTarget.className += " active";
 }
